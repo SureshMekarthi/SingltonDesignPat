d280d71f6ed15ba7d4ea009111cbd36cfa3af265
Suresh Mekarthi <sureshmekarthi@gmail.com>
2026-03-27 12:19:28 -0400

    Add project files.

---
+++ SingltonDesignPat/Singleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingltonDesignPat
+{
+    internal class Singleton
+    {
+        private static Singleton _instance;
+
+        private Singleton() { }
+
+        public static Singleton GetInstance()
+        {
+            if(_instance ==null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+    }
+}
